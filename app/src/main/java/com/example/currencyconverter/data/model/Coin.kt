2c5756e265cb2d064d01242ec05918e06ff63e98
Ayrton <ayrttoon@hotmail.com>
2022-01-14 20:14:54 -0300

    Dropdown values

---
+++ app/src/main/java/com/example/currencyconverter/data/model/Coin.kt
@@ -0,0 +1,13 @@
+package com.example.currencyconverter.data.model
+
+enum class Coin {
+    USD,
+    BRL,
+    CAD,
+    GBP,
+    ARS,
+    BTC,
+    EUR,
+    JPY,
+    CNY
+}
--- app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.currencyconverter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.widget.doAfterTextChanged
 import com.example.currencyconverter.R
+import com.example.currencyconverter.data.model.Coin
 import com.example.currencyconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        bindAdapters()
+        bindingListeners()
+    }
+
+    private fun bindAdapters() {
+        val list = Coin.values()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        binding.tvFrom.setAdapter(adapter)
+        binding.tvFrom.setText(Coin.BRL.name, false)
+        binding.tvTo.setAdapter(adapter)
+        binding.tvTo.setText(Coin.USD.name, false)
+    }
+
+    private fun bindingListeners() {
+        binding.tilValue.editText?.doAfterTextChanged {
+            binding.btnConvert.isEnabled = it != null && it.toString().isNotEmpty()
+        }
+
+        binding.btnConvert.setOnClickListener {
+
+        }
     }
 }
